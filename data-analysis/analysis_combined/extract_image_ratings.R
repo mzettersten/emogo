d0bbd26972da24566b1ac9c7b067ba0650ae3b1b
mzettersten <mzettersten@users.noreply.github.com>
2023-03-16 03:44:38 -0400

    quick update to image norms

--- data-analysis/analysis_combined/extract_image_ratings.R
+++ data-analysis/analysis_combined/extract_image_ratings.R
@@ -19,8 +19,33 @@ d <- d_v1 %>%
   full_join(d_pers) %>%
   full_join(d_psych)
 
+write_csv(d,here("data-analysis", "data","combined","emogo-combined-alldata-anonymized.csv"))
+
+image_response_d <- d %>%
+  rename(stimulus_image=simulus_image) %>%
+  filter((trial_type %in% c("survey-text"))&!is.na(stimulus_image)) %>%
+  select(task_version,subject,trial_type,stimulus_image,response) %>%
+  mutate(response_raw = map(response, ~ fromJSON(.) %>% as.data.frame())) %>%
+  unnest(response_raw) %>%
+  rename(response_raw=Q0) %>%
+  mutate(response_cleaned=trimws(tolower(response_raw))) %>%
+  select(-trial_type,-response)
+
 image_rating_d <- d %>%
   rename(stimulus_image=simulus_image) %>%
-  filter((trial_type %in% c("survey-text","html-slider-response"))&!is.na(stimulus_image)) %>%
+  filter((trial_type %in% c("html-slider-response"))) %>%
   select(task_version,subject,trial_type,stimulus_image,response,slider_start) %>%
-  mutate(response_clean = map(response, ~ fromJSON(.) %>% as.data.frame()))
+  select(-trial_type) %>%
+  rename(rating=response)
+
+image_norms_d <- image_response_d %>%
+  left_join(image_rating_d) %>%
+  mutate(stimulus=str_replace(stimulus_image,"stimuli/","")) %>%
+  mutate(stimulus=str_replace(stimulus,".jpg","")) %>%
+  separate(stimulus,into=c("model","emotion"),sep="_",remove=FALSE)
+
+write_csv(image_norms_d,here("data-analysis", "data","combined","emogo-combined-image-norms.csv"))
+
+temp <- image_norms_d %>%
+  group_by(emotion,response_cleaned) %>%
+  summarize(n=n())
